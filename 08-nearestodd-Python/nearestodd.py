89f1d16714975adfc00087776da77d4ad5fcff8c
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-14 18:46:37 +0530

    done

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -4,8 +4,23 @@
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
 
 
-
 def fun_nearestodd(n):
-	return 0
+	if type(n) == int:  
+		if n % 2 == 0:
+			return n - 1
+		else:
+			return n
+	else:  # case of a float
+		temp = int(n)  # the truncated integer of n
+		up_sub = abs(temp + 1 - n)
+		down_sub = abs(temp - 1 - n)
+		if temp % 2 != 0:  # if the truncated integer itself is an odd
+			return temp
+		else:  # if the truncated int is an even
+			if up_sub < down_sub:
+				return temp + 1
+			else:
+				return temp - 1
+	
 
 
