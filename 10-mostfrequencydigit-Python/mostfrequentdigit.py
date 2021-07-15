8519847455c2ac48389a40cb1cda94b45f35f896
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-15 21:05:17 +0530

    done

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,19 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+	a=0
+	b=0
+	for i in range(0,10):
+		f=count(n,i)
+		if(f>a):
+			a=f
+			b=i
+	return b
+def count(n,i):
+	c=0
+	while(n>0):
+		j=n%10
+		if(i==j):
+			c+=1
+		n=n//10
+	return c
