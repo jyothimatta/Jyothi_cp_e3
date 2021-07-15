0f7f817e346c53f6ad68a8869276bd9245cd00f4
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-15 22:06:13 +0530

    done

--- 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
+++ 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
@@ -37,8 +37,78 @@
 # assert(bonusPlayThreeDiceYahtzee(2633413) == (633, 16))
 # assert(bonusPlayThreeDiceYahtzee(2333413) == (333, 29))
 # assert(bonusPlayThreeDiceYahtzee(2333555) == (555, 35))
+def handToDice(hand):
+	firstDigit = hand // 100
+	secondDigit = (hand % 100) // 10
+	thirdDigit = (hand % 100) % 10
+	return firstDigit, secondDigit, thirdDigit
+
+def diceToOrderedHand(a, b, c):
+	Largest = max(a,b,c)
+	Smallest = min(a,b,c)
+	Medium = a + b + c - Largest - Smallest
+	return Largest * (10)**2 + Medium * 10 + Smallest
+
+def matchNum(hand):
+	digit1, digit2, digit3 = handToDice(hand)
+	if digit1 == digit2 == digit3:
+		return 3
+	elif digit1 != digit2 != digit3:
+		return 1
+	else:
+		return 2
+
+def playStep2(hand, dice):
+	getMatch = matchNum(hand)
+	a, b, c = handToDice(hand)
+	orderedHand= diceToOrderedHand(a, b, c)
+	# Case 1
+	if getMatch == 3:
+		return hand, dice
+	# Case 2
+	elif getMatch == 2:
+		num1, num2, num3 = handToDice(orderedHand)
+		# Select the last digit of the dice
+		num4 = dice % 10
+		if num1 == num2:
+			newHand = diceToOrderedHand(num1, num2, num4)
+			return newHand, dice // 10
+		elif num1 == num3:
+			newHand = diceToOrderedHand(num1, num3, num4)
+			return newHand, dice // 10
+		else:
+			newHand = diceToOrderedHand(num3, num2, num4)
+			return newHand, dice // 10
+	# Case 3
+	else:
+		num1, num2, num3 = handToDice(orderedHand)
+		secondHalf = dice % 100
+		t1, t2, t3 = handToDice(num1 * 100 + secondHalf)
+		new_hand = diceToOrderedHand(t1, t2, t3)
+		return new_hand, dice // 100
 
 
+def score(hand):
+	rank = matchNum(hand)
+	num1, num2, num3 = handToDice(hand)
+	if rank == 3:
+		return 20 + 3 * (hand // 100)
+	elif rank == 2:
+		if num1 == num2:
+			return 10 + 2 * num1
+		elif num1 == num3:
+			return 10 + 2 * num3
+		else:
+			return 10 + 2 * num2
+	else:
+		return max(num1, num2, num3)
+		
 def bonusplaythreediceyahtzee(dice):
-	# Your code goes here
-	pass
+	d = dice // 1000
+	hand = dice % 1000
+
+	hand1, dice1 = playStep2(hand, d)
+	hand2, dice2 = playStep2(hand1, dice1)
+
+	scr = score(hand2)
+	return hand2, scr
