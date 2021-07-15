5c8833063e75e637ce8db76ddc09d51934509541
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-15 21:28:14 +0530

    done

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -2,5 +2,19 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-	# Your code goes here
-	pass
+	less = []
+	equal = []
+	greater = []
+	if len(array) > 1:
+		pivot = array[0]
+		for x in array:
+			if x < pivot:
+				less.append(x)
+			elif x == pivot:
+				equal.append(x)
+			elif x > pivot:
+				greater.append(x)
+		return quicksort(less)+equal+quicksort(greater) 
+	else:  
+		return array
+
