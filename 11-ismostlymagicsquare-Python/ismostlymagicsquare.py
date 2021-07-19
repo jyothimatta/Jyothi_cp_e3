ac4fb8e1c31ae338afe4328211b73f62f5a43329
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-19 23:30:22 +0530

    Done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,18 +1,32 @@
-# isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
-# with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
-# Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
-# a "mostly magic" square, including this sample magic square:
-# Read for more: https://en.wikipedia.org/wiki/Magic_square
-# Here is another mostly-magic square:
-# [ [ 42 ]]
-# That square is 1x1 and each row, column, and diagonal sums to 42! And finally, here is a not-mostly-magic square:
-# [ [ 1, 2],
-#   [ 2, 1]]
-# Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
+"""isMostlyMagicSquare(a) [15 pts]
+Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
+Each row, each column, and each of the 2 diagonals each sum to the same total.
+A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
+from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+a "mostly magic" square, including this sample magic square:
+Read for more: https://en.wikipedia.org/wiki/Magic_square
+Here is another mostly-magic square:
+[ [ 42 ]]
+That square is 1x1 and each row, column, and diagonal sums to 42! And finally, here is a not-mostly-magic square:
+[ [ 1, 2],
+  [ 2, 1]]
+Each row and each column add to 3, but one diagonal adds to 2 and the other to 4."""
 
 def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+	iSize = len(a[0])
+	sum_list = []
+	sum_list.extend([sum (lines) for lines in a])   
+	for col in range(iSize):
+		sum_list.append(sum(row[col] for row in a))
+	result1 = 0
+	for i in range(0,iSize):
+		result1 +=a[i][i]
+	sum_list.append(result1)  
+	result2 = 0
+	for i in range(iSize-1,-1,-1):
+		result2 +=a[i][i]
+	sum_list.append(result2)
+	if len(set(sum_list))>1:
+		return False
+	return True
