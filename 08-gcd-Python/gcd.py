95d8f4f012757dd0e29e7c933d0d185dca752620
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-07-23 21:00:20 +0530

    done

--- 08-gcd-Python/gcd.py
+++ 08-gcd-Python/gcd.py
@@ -10,4 +10,13 @@
 
 def gcd(m, n):
 	# your code goes here
-	pass
+	gcd=1
+	if m > n:
+		small = n
+	else:
+		small = m
+	for i in range(1, small+1):
+		if((m % i == 0) and (n % i == 0)):
+			gcd = i
+			  
+	return gcd
