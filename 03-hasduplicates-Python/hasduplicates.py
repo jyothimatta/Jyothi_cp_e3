49fdc5803391c58b9a041154f08e793b3930aa12
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-08-03 20:42:21 +0530

    done

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -3,6 +3,16 @@
 # contains any duplicate values (that is, 
 # if any two values in L are equal to each other), and False otherwise.
 
+import itertools
+#num = [[10, 20], [40], [30, 56, 25], [10, 20], [33], [40]]
+#print("Original List", num)
+#num.sort()
+#new_num = list(num for num,_ in itertools.groupby(num))
+#print("New List", new_num)
 def hasduplicates(L):
-	# Your code goes here
-	pass
+    for i in range(len(L)-1):
+        for j in range(len(L[i])):
+            for h in range(i+1,len(L)):
+                if L[i][j] in L[h]:
+                    return True
+    return False
