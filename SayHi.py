069013eee9c1329d4834e99fa6680d6813567645
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-08-04 15:57:39 +0530

    done

--- SayHi.py
+++ SayHi.py
@@ -3,6 +3,19 @@
 
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
+class Dog(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-class SayHi(Object):
-    pass
+    # Now we are using self, as convention requires:
+    def sayHi(self):
+        print(f'Hi, my name is {self.name} and I am {self.age} years old!')
+
+d1 = Dog('Dot', 4)
+d2 = Dog('Elf', 3)
+
+# Notice how we change the function calls into method calls:
+
+d1.sayHi() # Hi, my name is Dot and I am 4 years old!
+d2.sayHi() # Hi, my name is Elf and I am 3 years old!
