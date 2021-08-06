d0821894221fd9f63209cd3a313671fe7e86da06
jyothimatta <71481815+jyothimatta@users.noreply.github.com>
2021-08-06 13:18:34 +0530

    done

--- 04-vowelscount-Java/MyString.java
+++ 04-vowelscount-Java/MyString.java
@@ -4,10 +4,37 @@
 // So, for example, ("Abc def!!! a? yzyzyz!") returns 3 (two a's and one e).
 
 
+import java.io.*;
+  
 public class MyString {
-	public int vowelCount(String s){
-		// Your code goes here...
-		return 0;
-	}
-	
+    public static void main(String[] args)
+        throws IOException
+    {
+        String str = "GeeksForGeeks";
+  
+            str = str.toLowerCase();
+  
+        // toCharArray() is used to convert 
+        // string to char array
+        // char[] chars = str.toCharArray();
+  
+        int count = 0;
+  
+        for (int i = 0; i < str.length(); i++)
+        {
+            // check if char[i] is vowel
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e'
+                || str.charAt(i) == 'i'
+                || str.charAt(i) == 'o'
+                || str.charAt(i) == 'u')
+            {
+                // count increments if there is vowel in
+                // char[i]
+                count++;
+            }
+        }
+  
+        // display total count of vowels in string
+        System.out.println("Total no of vowels in string are: " + count);
+    }
 }
